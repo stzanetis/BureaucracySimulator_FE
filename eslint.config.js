9c077a97508246305b9f0cad82ce57e4398b7de7
Vasilis Zoidis <billzoidis@gmail.com>
2025-12-14 21:02:41 +0200

    workflow test final?

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,6 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
-    ignores: ['cypress/**', 'cypress.config.js'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -25,28 +24,6 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-  {
-    files: ['cypress/**/*.js', 'cypress.config.js'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        cy: 'readonly',
-        Cypress: 'readonly',
-        describe: 'readonly',
-        context: 'readonly',
-        it: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        before: 'readonly',
-        after: 'readonly',
-        expect: 'readonly',
-      },
-    },
-    rules: {
-      'no-unused-vars': 'off',
     },
   },
 ])
